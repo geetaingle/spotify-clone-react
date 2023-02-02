514372207aaa9e1c18f95e706745320907036a41
Geeta Ingle <geeta.i@softlinklocal.com>
2023-02-02 19:00:10 +0530

    Login with spotify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { reducerCases } from "./utils/Constants";
+import { useStateProvider } from "./utils/StateProvider";
 
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+  return <>{token ? <Spotify /> : <Login />}</>;
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,57 @@
 import styled from "styled-components";
+
 function Login() {
-  return <Container>Login</Container>;
+  const handleClick = () => {
+    const clientId = "c56e948bb1db4e339fcfc29bbe95e2d6";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-email",
+      "user-read-private",
+      "user-read-playback-state",
+      "user-modify-playback-state",
+      "user-read-currently-playing",
+      "user-read-playback-position",
+      "user-top-read",
+      "user-read-recently-played",
+    ];
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_daialog=true`;
+  };
+
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt="Spotify-logo"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+  );
 }
 
 export default Login;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    background-color: black;
+    color: #49f585;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
