0c5721ffac4a2fb781c91ca87ed2e4727538339a
Geeta Ingle <42793643+geetaingle@users.noreply.github.com>
2023-02-13 00:02:17 +0530

    Navbar

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,5 @@
 export const reducerCases = {
   SET_TOKEN: "SET_TOKEN",
   SET_PLSYLISTS: "SET_PLAYLISTS",
+  SET_USER: "SET_USER"
 };
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -3,6 +3,7 @@ import { reducerCases } from "./Constants";
 export const initialState = {
   token: null,
   playlists: [],
+  userInfo: null
 };
 
 const reducer = (state, action) => {
@@ -18,6 +19,12 @@ const reducer = (state, action) => {
         ...state,
         playlists: action.playlists,
       };
+    }
+    case reducerCases.SET_USER: {
+      return {
+        ...state,
+        userInfo: action.userInfo
+      }
     }
     default:
       return state;
