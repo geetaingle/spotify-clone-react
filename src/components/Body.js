06545916b8c56e2efcc388cdefc8783e24559303
Geeta Ingle <geeta.i@softlinklocal.com>
2023-02-13 19:37:51 +0530

    Spotify Body - WIP

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import { AiFillClockCircle } from "react-icons/ai";
+import { useStateProvider } from "../utils/StateProvider";
+import { useEffect } from "react";
+import axios from "axios";
+
 function Body() {
-  return <>Body</>;
+  const [{ token, selectedPlaylistId }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const getInitialPlaylist = async () => {
+      const response = axios.get(
+        `https://api.spotify.com/v1/playlists/${
+          (selectedPlaylistId,
+          {
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-type": "application/json",
+            },
+          })
+        }`
+      );
+    };
+    getInitialPlaylist();
+  }, [token, dispatch]);
+  return <Container></Container>;
 }
 
 export default Body;
+
+const Container = styled.div``;
