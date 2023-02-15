2221852a3cb27e5fd3b67697ded1d2a7d9fd14ce
Geeta Ingle <geeta.i@softlinklocal.com>
2023-02-15 13:39:22 +0530

    spotify body css

--- src/components/Body.js
+++ src/components/Body.js
@@ -74,46 +74,46 @@ function Body() {
                 </span>
               </div>
             </div>
-          </div>
-          <div>
-            {selectedPlaylist.tracks.map(
-              (
-                {
-                  id,
-                  name,
-                  artists,
-                  image,
-                  duration,
-                  album,
-                  context_uri,
-                  track_number,
-                },
-                index
-              ) => {
-                return (
-                  <div className="row" key={id}>
-                    <div className="col">
-                      <span>{index + 1}</span>
-                    </div>
-                    <div className="col detail">
-                      <div className="image">
-                        <img src={image} alt="track" />
-                      </div>
-                      <div className="info">
-                        <span className="name">{name}</span>
-                        <span>{artists}</span>
-                      </div>
+            <div className="tracks">
+              {selectedPlaylist.tracks.map(
+                (
+                  {
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },
+                  index
+                ) => {
+                  return (
+                    <div className="row" key={id}>
                       <div className="col">
-                        <span>{album}</span>
+                        <span>{index + 1}</span>
                       </div>
-                      <div className="col">
-                        <span>{duration}</span>
+                      <div className="col detail">
+                        <div className="image">
+                          <img src={image} alt="track" />
+                        </div>
+                        <div className="info">
+                          <span className="name">{name}</span>
+                          <span>{artists}</span>
+                        </div>
+                        <div className="col">
+                          <span>{album}</span>
+                        </div>
+                        <div className="col">
+                          <span>{duration}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              }
-            )}
+                  );
+                }
+              )}
+            </div>
           </div>
         </>
       )}
@@ -123,4 +123,69 @@ function Body() {
 
 export default Body;
 
-const Container = styled.div``;
+const Container = styled.div`
+  .playlist {
+    margin: 0 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    .image {
+      img {
+        height: 15rem;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 25px;
+      }
+    }
+    .details {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      color: #e0dede;
+      .title {
+        color: white;
+        font-size: 4rem;
+      }
+    }
+  }
+  .list {
+    .header__row {
+      display: grid;
+      grid-template-column: 0.3fr 3fr 2fr 0.1fr;
+      color: #dddcdc;
+      margin: 1rem 0 0 0;
+      position: sticky;
+      top: 15vh;
+      padding: 1rem 3rem;
+      transition: 0.3s ease-in-out;
+    }
+    .tracks {
+      margin: 0 2rem;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 5rem;
+      .row {
+        padding: 0.5rem 1rem;
+        display: grid;
+        grid-template-columns: 0.3fr 3.1fr 1.9fr 0.1fr;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+        .col {
+          display: flex;
+          align-items: center;
+          color: #dddcdc;
+          img {
+            height: 40px;
+          }
+        }
+        .details {
+          display: flex;
+          gap: 1rem;
+          .info {
+            display: flex;
+            flex-direction: column;
+          }
+        }
+      }
+    }
+  }
+`;
